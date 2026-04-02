90656509f20323a2851e59c52d58561586ab511b
Yang Wei <yangwei3@lixiang.com>
2026-04-02 09:49:16 +0800

    Add PyMySQL and refresh generated lockfiles

--- app/models.py
+++ app/models.py
@@ -89,7 +89,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     confirmed = db.Column(db.Boolean, default=False)
     name = db.Column(db.String(64))
     location = db.Column(db.String(64))
